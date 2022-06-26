8fa535c95bd701e6555d984fecbfcb88fa873e11
seungyeon.bae <bsyeon015@gmail.com>
2022-06-26 17:40:47 +0900

    🧙‍♂️ 장바구니 담기, 삭제 기능 추가 (버튼 클릭)

--- index.js
+++ index.js
@@ -12,35 +12,39 @@ menuList.addEventListener('mouseover', function(e) {
     }
 });
 
+// 검색어 하이라이트 함수
+function SearchWordHighlights(title) {
+    let regex = new RegExp(searchGoods.value, 'g');
+        title = title.replace(regex, `<span class='highlight'>${searchGoods.value}</span>`);
+
+    return title;
+}
+
+
 // 상품 리스트 출력 함수
 function showGoodsList(element) {
-    // 검색어 하이라이트
     let title = element.title;
 
     if (searchGoods.value.length > 0) {
-        let regex = new RegExp(searchGoods.value, 'g');
-        title = title.replace(regex, `<span class='highlight'>${searchGoods.value}</span>`);
+        title = SearchWordHighlights(title);
     }
 
     // HTML 작성
-    return `<article class="goods" draggable="true">
-                <div class="goods-image">
+    return `<article class="goods" draggable="true" ondragstart="drag(event)" data-id="${element.id}">
+                <div class="goods-photo">
                     <img src="./assets/${element.photo}" alt="${element.brand} ${element.title}" />
                 </div>
                 <div class="goods-contents">
-                    <p id="goods-id">
-                        ${element.id}
-                    </p>
                     <h1 class="goods-title">
                         ${title}
                     </h1>
-                    <p class="goods-company">
+                    <p class="goods-brand">
                         ${element.brand}
                     </p>
                     <p class="goods-price">
                         가격 : <span>${element.price}</span>
                     </p>
-                    <button type="button" class="fill-button">
+                    <button type="button" class="fill-button add-cart">
                         장바구니 담기
                     </button>
                 </div>
@@ -57,7 +61,7 @@ let goodsWrapper = document.getElementsByClassName('goods-wrapper')[0];
 let goods = document.getElementsByClassName('goods');
 let goodsData = '';
 
-function dataFetch() {
+function fetchGoodsList() {
     let searchGoods = document.getElementById('search-goods');
 
     fetch('./assets/store.json')
@@ -104,9 +108,178 @@ let searchGoods = document.getElementById('search-goods');
 
 searchGoods.addEventListener('change', function() {
     resetGoodsList();
-    dataFetch();
+    fetchGoodsList();
 });
 
 window.onload = function() {
-    dataFetch();
+    // 상품 목록 출력
+    fetchGoodsList();
+
+    // 장바구니 목록 출력
+    if (localStorage.getItem('shoppingCart') != null) {
+        let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
+
+        resetCart();
+        shoppingCart.forEach(function(element) {
+            let goodsInCartHTML = showCart(element);
+            cart.insertAdjacentHTML('beforeend', goodsInCartHTML);
+        });
+    }
+}
+
+// 장바구니 담기 기능
+// 1. 장바구니 담기 버튼 클릭하여 담기
+//  1) 장바구니 담기 버튼을 클릭하면
+//  2) 로컬스토리지에 추가하고
+//  3) 장바구니 화면에 출력
+goodsWrapper.addEventListener('click', function(e) {
+    if (e.target.getAttribute('class').includes('add-cart')) {
+        // 클릭한 상품 정보 찾기
+        let id = e.target.parentNode.parentNode.getAttribute('data-id');
+        let photo = e.target.parentNode.parentNode.childNodes[1].childNodes[1].getAttribute('src');
+        let title = e.target.parentNode.childNodes[1].innerText;
+        let brand = e.target.parentNode.childNodes[3].innerText;
+        let price = e.target.parentNode.childNodes[5].childNodes[1].innerText;
+        let shoppingCart = new Array();
+        
+        // 등록된 상품이 있는 경우
+        if (localStorage.getItem('shoppingCart') != null) {
+            shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
+            let isDuplicateAddCart = false;
+
+            // 중복 클릭인 경우
+            for (let i = 0; i < shoppingCart.length; i++) {
+                if (id == shoppingCart[i].id) {
+                    shoppingCart[i].quantity = shoppingCart[i].quantity + 1;
+                    isDuplicateAddCart = true;
+                    break;
+                }
+            }
+
+            if (isDuplicateAddCart == false) {
+                shoppingCart.push({
+                    'id': id,
+                    'photo': photo,
+                    'title': title,
+                    'brand': brand,
+                    'price': price,
+                    'quantity': 1
+                });
+            }
+
+            localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+        } else {
+            // 로컬스토리지에 상품 정보 최초 추가 하기
+            localStorage.setItem(
+                'shoppingCart',
+                JSON.stringify([
+                    {
+                        'id': id,
+                        'photo': photo,
+                        'title': title,
+                        'brand': brand,
+                        'price': price,
+                        'quantity': 1
+                    }
+                ])
+            );
+
+            shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
+            cart.classList.add('haveGoods');
+        }
+
+        resetCart();
+        shoppingCart.forEach(function(element) {
+            let goodsInCartHTML = showCart(element);
+            cart.insertAdjacentHTML('beforeend', goodsInCartHTML);
+        });
+    }
+});
+
+// 장바구니 담기 기능
+// 2. 드래그 앤 드롭 이용하여 담기
+//  1) 상품 드래그해서 장바구니 근처로 오면
+//  2) 장바구니 목록에 추가
+//  3) 장바구니 목록에 추가 시 장바구니 담기 버튼을 수량 출력 text로 변경
+//     - 이미 있는 상품인 경우 수량++
+let cart = document.getElementsByClassName('cart')[0];
+
+function drag(ev) {
+    ev.dataTransfer.setData('target-class', ev.target.className);
+    ev.dataTransfer.setData('target-id', ev.target.dataset.id);
+}
+
+function dragEnter(ev) {
+    ev.preventDefault(); // 이벤트의 기본값을 최소화
+}
+
+function drop(ev) {
+    ev.preventDefault(); // 이벤트의 기본값을 최소화
+
+    let targetClass = ev.dataTransfer.getData('target-class'); // 드래그 앤 드롭의 대상이 되는 데이터를 가져옴
+    let targetId = ev.dataTransfer.getData('target-id');
+    let targetHTML = document.getElementsByClassName(targetClass)[targetId].cloneNode(true);
+    
+    ev.target.appendChild(targetHTML);
+}
+
+// 장바구니 제거 기능
+cart.addEventListener('click', function(e) {
+    if (e.target.getAttribute('class').includes('delete-cart')) {
+        let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
+        let removeGoodsId = e.target.parentNode.parentNode.getAttribute('data-id');
+
+        // 삭제한 상품 array에서 제외
+        shoppingCart = shoppingCart.filter(element => {
+            return element.id !== removeGoodsId;
+        });
+        
+        // 상품이 0개면 array 자체를 삭제한 후 화면 초기화
+        if (shoppingCart.length == 0) {
+            localStorage.removeItem('shoppingCart');
+            cart.innerHTML = `<span>원하는 상품을 여기로 드래그 하세요 💚</span>`;
+            cart.classList.remove('haveGoods');
+
+            return;
+        }
+        
+        localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+
+        resetCart();
+        shoppingCart.forEach(function(element) {
+            let goodsInCartHTML = showCart(element);
+            cart.insertAdjacentHTML('beforeend', goodsInCartHTML);
+        });
+    }
+});
+
+// 장바구니 화면 추가 함수
+function showCart(element) {
+    return `<article class="goods" data-id="${element.id}">
+        <div class="goods-photo">
+            <img src="${element.photo}" alt="${element.brand} ${element.title}" />
+        </div>
+        <div class="goods-contents">
+            <h1 class="goods-title">
+                ${element.title}
+            </h1>
+            <p class="goods-brand">
+                ${element.brand}
+            </p>
+            <p class="goods-price">
+                가격 : <span>${element.price}</span>
+            </p>
+            <p class="goods-quantity">
+                ${element.quantity}
+            </p>
+            <button type="button" class="fill-button delete-cart">
+                제거
+            </button>
+        </div>
+    </article>`;
+}
+
+// 장바구니 화면 리셋 함수
+function resetCart() {
+    cart.innerHTML = '';
 }
