512fd1b128a8b9dfca10ca3488185c63187ca536
seungyeon.bae <bsyeon015@gmail.com>
2022-06-27 21:35:31 +0900

    🔧 장바구니 추가 기능 오류 수정

--- index.js
+++ index.js
@@ -12,61 +12,19 @@ menuList.addEventListener('mouseover', function(e) {
     }
 });
 
-// 검색어 하이라이트 함수
-function SearchWordHighlights(title) {
-    let regex = new RegExp(searchGoods.value, 'g');
-        title = title.replace(regex, `<span class='highlight'>${searchGoods.value}</span>`);
-
-    return title;
-}
-
-
-// 상품 리스트 출력 함수
-function showGoodsList(element) {
-    let title = element.title;
-
-    if (searchGoods.value.length > 0) {
-        title = SearchWordHighlights(title);
-    }
-
-    // HTML 작성
-    return `<article class="goods" draggable="true" ondragstart="drag(event)" data-id="${element.id}">
-                <div class="goods-photo">
-                    <img src="./assets/${element.photo}" alt="${element.brand} ${element.title}" />
-                </div>
-                <div class="goods-contents">
-                    <h1 class="goods-title">
-                        ${title}
-                    </h1>
-                    <p class="goods-brand">
-                        ${element.brand}
-                    </p>
-                    <p class="goods-price">
-                        가격 : <span>${element.price}</span>
-                    </p>
-                    <button type="button" class="fill-button add-cart">
-                        장바구니 담기
-                    </button>
-                </div>
-            </article>`;
-}
-
-// 상품 리스트 리셋 함수
-function resetGoodsList() {
-    goodsWrapper.innerHTML = '';
-}
-
 // 상품 리스트 가져와서 화면에 출력
 let goodsWrapper = document.getElementsByClassName('goods-wrapper')[0];
 let goods = document.getElementsByClassName('goods');
-let goodsData = '';
+let goodsData = new Array();
 
 function fetchGoodsList() {
     let searchGoods = document.getElementById('search-goods');
 
     fetch('./assets/store.json')
         .then(res => res.json())
-        .then(data =>
+        .then(data => {
+            goodsData = data; //원본 데이터 저장
+
             data.products.forEach(function(element) {
                 let title = element.title;
                 
@@ -77,7 +35,7 @@ function fetchGoodsList() {
                 let goodsHTML = showGoodsList(element);
                 goodsWrapper.insertAdjacentHTML('beforeend', goodsHTML);
             })
-        )
+        })
         .catch(function(error) {
             console.log(error);
         });
@@ -138,62 +96,11 @@ goodsWrapper.addEventListener('click', function(e) {
     if (e.target.getAttribute('class').includes('add-cart')) {
         // 클릭한 상품 정보 찾기
         let id = e.target.parentNode.parentNode.getAttribute('data-id');
-        let photo = e.target.parentNode.parentNode.childNodes[1].childNodes[1].getAttribute('src');
-        let title = e.target.parentNode.childNodes[1].innerText;
-        let brand = e.target.parentNode.childNodes[3].innerText;
-        let price = e.target.parentNode.childNodes[5].childNodes[1].innerText;
-        let shoppingCart = new Array();
-        
-        // 등록된 상품이 있는 경우
-        if (localStorage.getItem('shoppingCart') != null) {
-            shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
-            let isDuplicateAddCart = false;
 
-            // 중복 클릭인 경우
-            for (let i = 0; i < shoppingCart.length; i++) {
-                if (id == shoppingCart[i].id) {
-                    shoppingCart[i].quantity = shoppingCart[i].quantity + 1;
-                    isDuplicateAddCart = true;
-                    break;
-                }
+        goodsData.products.forEach(element => {
+            if (element.id == id) {
+                addGoodsInShopingCart(element);
             }
-
-            if (isDuplicateAddCart == false) {
-                shoppingCart.push({
-                    'id': id,
-                    'photo': photo,
-                    'title': title,
-                    'brand': brand,
-                    'price': price,
-                    'quantity': 1
-                });
-            }
-
-            localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
-        } else {
-            // 로컬스토리지에 상품 정보 최초 추가 하기
-            localStorage.setItem(
-                'shoppingCart',
-                JSON.stringify([
-                    {
-                        'id': id,
-                        'photo': photo,
-                        'title': title,
-                        'brand': brand,
-                        'price': price,
-                        'quantity': 1
-                    }
-                ])
-            );
-
-            shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
-            cart.classList.add('haveGoods');
-        }
-
-        resetCart();
-        shoppingCart.forEach(function(element) {
-            let goodsInCartHTML = showCart(element);
-            cart.insertAdjacentHTML('beforeend', goodsInCartHTML);
         });
     }
 });
@@ -207,7 +114,6 @@ goodsWrapper.addEventListener('click', function(e) {
 let cart = document.getElementsByClassName('cart')[0];
 
 function drag(ev) {
-    ev.dataTransfer.setData('target-class', ev.target.className);
     ev.dataTransfer.setData('target-id', ev.target.dataset.id);
 }
 
@@ -218,71 +124,147 @@ function dragEnter(ev) {
 function drop(ev) {
     ev.preventDefault(); // 이벤트의 기본값을 최소화
 
-    let targetClass = ev.dataTransfer.getData('target-class'); // 드롭 이벤트 대상의 class name 가져옴
     let targetId = ev.dataTransfer.getData('target-id'); // 드롭 이벤트 대상의 id 가져옴
 
-    // 드롭 이벤트 대상 clone
-    let targetHTML = document.getElementsByClassName(targetClass)[targetId].cloneNode(true);
-
-    // 드롭 이벤트 대상을 장바구니에 추가하는 함수 호출
-    targetHTML = addShopingCartByDropItem(targetHTML);
+    goodsData.products.forEach(element => {
+        if (element.id == targetId) {
+            addGoodsInShopingCart(element);
+        }
+    })
 }
 
-// 드롭 이벤트 대상을 장바구니에 추가하는 함수
-function addShopingCartByDropItem(target) {
-     // 드롭 이벤트 대상의 상품 정보 찾기
-     let id = target.getAttribute('data-id');
-     let photo = target.childNodes[1].childNodes[1].getAttribute('src');
-     let title = (target.childNodes[3].childNodes[1].innerText).trim();
-     let brand = (target.childNodes[3].childNodes[3].innerText).trim();
-     let price = target.childNodes[3].childNodes[5].childNodes[1].innerText;
-     let shoppingCart = new Array();
-
-     // 등록된 상품이 있는 경우
-     if (localStorage.getItem('shoppingCart') != null) {
-        shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
-        let isDuplicateAddCart = false;
-
-        // 중복 클릭인 경우
-        for (let i = 0; i < shoppingCart.length; i++) {
-            if (id == shoppingCart[i].id) {
-                shoppingCart[i].quantity = shoppingCart[i].quantity + 1;
-                isDuplicateAddCart = true;
-                break;
-            }
-        }
-
-        if (isDuplicateAddCart == false) {
-            shoppingCart.push({
-                'id': id,
-                'photo': photo,
-                'title': title,
-                'brand': brand,
-                'price': price,
-                'quantity': 1
-            });
-        }
-
-        localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
-    } else {
-        // 로컬스토리지에 상품 정보 최초 추가 하기
-        localStorage.setItem(
-            'shoppingCart',
-            JSON.stringify([
-                {
-                    'id': id,
-                    'photo': photo,
-                    'title': title,
-                    'brand': brand,
-                    'price': price,
-                    'quantity': 1
-                }
-            ])
-        );
-
-        shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
-        cart.classList.add('haveGoods');
+// 장바구니 제거 기능
+cart.addEventListener('click', function(e) {
+    if (e.target.getAttribute('class').includes('delete-cart')) {
+        deleteGoodsInCart(e.target.parentNode.parentNode.getAttribute('data-id'));
     }
+});
+
+// 검색어 하이라이트 함수
+function SearchWordHighlights(title) {
+    let regex = new RegExp(searchGoods.value, 'g');
+        title = title.replace(regex, `<span class='highlight'>${searchGoods.value}</span>`);
+
+    return title;
+}
+
+// 상품 리스트 출력 함수
+function showGoodsList(element) {
+    let title = element.title;
+
+    if (searchGoods.value.length > 0) {
+        title = SearchWordHighlights(title);
+    }
+
+    // HTML 작성
+    return `<article class="goods" draggable="true" ondragstart="drag(event)" data-id="${element.id}">
+                <div class="goods-photo">
+                    <img src="./assets/${element.photo}" alt="${element.brand} ${element.title}" />
+                </div>
+                <div class="goods-contents">
+                    <h1 class="goods-title">
+                        ${title}
+                    </h1>
+                    <p class="goods-brand">
+                        ${element.brand}
+                    </p>
+                    <p class="goods-price">
+                        가격 : <span>${element.price}</span>
+                    </p>
+                    <button type="button" class="fill-button add-cart">
+                        장바구니 담기
+                    </button>
+                </div>
+            </article>`;
+}
+
+// 상품 리스트 리셋 함수
+function resetGoodsList() {
+    goodsWrapper.innerHTML = '';
+}
+
+// 상품을 장바구니에 추가하는 함수
+function addGoodsInShopingCart(target) {
+    // 상품 정보 찾기
+    let id = target.id;
+    let photo = target.photo;
+    let title = target.title;
+    let brand = target.brand;
+    let price = target.price;
+    let shoppingCart = new Array();
+
+    // 등록된 상품이 있는 경우
+    if (localStorage.getItem('shoppingCart') != null) {
+       shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
+       let isDuplicateAddCart = false;
+
+       // 중복 선택인 경우
+       for (let i = 0; i < shoppingCart.length; i++) {
+           if (id == shoppingCart[i].id) {
+               shoppingCart[i].quantity = shoppingCart[i].quantity + 1;
+               isDuplicateAddCart = true;
+               break;
+           }
+       }
+
+       if (isDuplicateAddCart == false) {
+           shoppingCart.push({
+               'id': id,
+               'photo': photo,
+               'title': title,
+               'brand': brand,
+               'price': price,
+               'quantity': 1
+           });
+       }
+
+       localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+   } else {
+       // 로컬스토리지에 상품 정보 최초 추가 하기
+       localStorage.setItem(
+           'shoppingCart',
+           JSON.stringify([
+               {
+                   'id': id,
+                   'photo': photo,
+                   'title': title,
+                   'brand': brand,
+                   'price': price,
+                   'quantity': 1
+               }
+           ])
+       );
+
+       shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
+       cart.classList.add('haveGoods');
+   }
+
+   resetCart();
+   shoppingCart.forEach(function(element) {
+       let goodsInCartHTML = showCart(element);
+       cart.insertAdjacentHTML('beforeend', goodsInCartHTML);
+   });
+}
+
+// 상품을 장바구니에서 제거하는 함수
+function deleteGoodsInCart(targetId) {
+    let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
+
+    // 삭제한 상품 array에서 제외
+    shoppingCart = shoppingCart.filter(element => {
+        return element.id !== Number(targetId);
+    });
+    
+    // 상품이 0개면 array 자체를 삭제한 후 화면 초기화
+    if (shoppingCart.length == 0) {
+        localStorage.removeItem('shoppingCart');
+        cart.innerHTML = `<span>원하는 상품을 여기로 드래그 하세요 💚</span>`;
+        cart.classList.remove('haveGoods');
+
+        return;
+    }
+    
+    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
 
     resetCart();
     shoppingCart.forEach(function(element) {
@@ -291,41 +273,11 @@ function addShopingCartByDropItem(target) {
     });
 }
 
-// 장바구니 제거 기능
-cart.addEventListener('click', function(e) {
-    if (e.target.getAttribute('class').includes('delete-cart')) {
-        let shoppingCart = JSON.parse(localStorage.getItem('shoppingCart'));
-        let removeGoodsId = e.target.parentNode.parentNode.getAttribute('data-id');
-
-        // 삭제한 상품 array에서 제외
-        shoppingCart = shoppingCart.filter(element => {
-            return element.id !== removeGoodsId;
-        });
-        
-        // 상품이 0개면 array 자체를 삭제한 후 화면 초기화
-        if (shoppingCart.length == 0) {
-            localStorage.removeItem('shoppingCart');
-            cart.innerHTML = `<span>원하는 상품을 여기로 드래그 하세요 💚</span>`;
-            cart.classList.remove('haveGoods');
-
-            return;
-        }
-        
-        localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
-
-        resetCart();
-        shoppingCart.forEach(function(element) {
-            let goodsInCartHTML = showCart(element);
-            cart.insertAdjacentHTML('beforeend', goodsInCartHTML);
-        });
-    }
-});
-
 // 장바구니 화면 추가 함수
 function showCart(element) {
     return `<article class="goods" data-id="${element.id}">
         <div class="goods-photo">
-            <img src="${element.photo}" alt="${element.brand} ${element.title}" />
+            <img src="./assets/${element.photo}" alt="${element.brand} ${element.title}" />
         </div>
         <div class="goods-contents">
             <h1 class="goods-title">
